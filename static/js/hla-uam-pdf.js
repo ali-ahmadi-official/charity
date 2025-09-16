77383e0bc8c73b831af1f4d3acb6255460260f50
ali-ahmadi-official <alihmad.1384@gmail.com>
2025-09-16 17:21:21 +0330

    add uam option

--- static/js/hla-uam-pdf.js
+++ static/js/hla-uam-pdf.js
@@ -22,3 +22,39 @@ document.querySelectorAll('.file-input').forEach(input => {
         fileName.textContent = '';                
     });
 });
+
+document.getElementById('class_i_input').addEventListener('change', function () {
+    const file = this.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('/api/extract/', {
+        method: 'POST',
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        // پر کردن فیلدها با داده‌های برگشتی
+        document.getElementById('first_name').value = data.first_name || '';
+        document.getElementById('last_name').value = data.last_name || '';
+        document.getElementById('national_code').value = data.national_code || '';
+        document.getElementById('gender').value = data.gender || '';
+        document.getElementById('pregnancies_number').value = data.pregnancies_number ?? '';
+        document.getElementById('age').value = data.age || '';
+        document.getElementById('blood_group').value = data.blood_group || '';
+
+        document.getElementById('first_name').style.border = "1px solid #f97316";
+        document.getElementById('last_name').style.border = "1px solid #f97316";
+        document.getElementById('national_code').style.border = "1px solid #f97316";
+        document.getElementById('gender').style.border = "1px solid #f97316";
+        document.getElementById('pregnancies_number').style.border = "1px solid #f97316";
+        document.getElementById('age').style.border = "1px solid #f97316";
+        document.getElementById('blood_group').style.border = "1px solid #f97316";
+
+        document.getElementById('api-message').style.display = 'flex';
+    })
+    .catch(err => {
+        console.error('❌ خطا در دریافت اطلاعات از API:', err);
+        alert('مشکلی در خواندن فایل PDF پیش آمد.');
+    });
+});
